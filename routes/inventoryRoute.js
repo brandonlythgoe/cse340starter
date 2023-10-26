a5fae0349ce95e39223fa9219e0ba0fcc25493b5
brandonlythgoe <lyt19002j@byui.edu>
2023-10-26 15:45:55 -0600

    final update

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,9 +2,11 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 // Define a route to retrieve a specific vehicle by ID
-router.get("/detail/:invId", invController.getInventoryItemDetail);
+router.get("/detail/:invId", utilities.handleErrors(invController.getInventoryItemDetail));
+
 module.exports = router;
 
